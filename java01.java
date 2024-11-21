7c24d1de1e5c9a8b22e521cd514713d793431bc6
Kecchi <mraflydharmanda@gmail.com>
2024-11-21 12:36:09 +0700

    first commit

---
+++ java01.java
@@ -0,0 +1,10 @@
+package sololearn;
+
+public class java01 {
+  public static void main(String[] args) {
+    float a = 10;
+    float b = 5;
+    float hasil = a/b;
+    System.out.println("hasil = " + hasil);
+  }
+}
